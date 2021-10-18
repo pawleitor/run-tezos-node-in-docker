c1973454f43918430b1ae9b84ce30f6bf8a1de02
tezdealer <92530297+tezdealer@users.noreply.github.com>
2021-10-18 12:18:23 -0400

    Update node-setup-from-snapshot.sh
    Add connection limits to config

--- node-setup-from-snapshot.sh
+++ node-setup-from-snapshot.sh
@@ -7,6 +7,7 @@ echo 'if data dir does not exist, create config and import snapshot'
 && echo 'initialize node' \
 && tezos-node config --config-file=$CONFIG --network=$NETWORK init \
 && tezos-node config --config-file=$CONFIG --rpc-addr=0.0.0.0:$RPC_PORT update \
+&& tezos-node config --config-file=$CONFIG --connections=100 --max-download-speed=1024 --max-upload-speed=1024 update \
 && echo 'always get new snapshot on rebiuld' \
 && wget $SNAPSHOT_URL -O $SNAPSHOT_PATH/$SNAPSHOT_FILE \
 && tezos-node snapshot import $SNAPSHOT_PATH/$SNAPSHOT_FILE --data-dir $DATA_DIR \
