a0599df2c077b75ddb2f3869aa753fa6059cb2e6
tezdealer <92530297+tezdealer@users.noreply.github.com>
2021-10-19 14:28:56 -0400

    Update node-setup-from-snapshot.sh
    Do separate checks for data and for config.json to make it easier to adjust configuration quickly as needed

--- node-setup-from-snapshot.sh
+++ node-setup-from-snapshot.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 CONFIG=$TEZOS_NODE_DATA/data/config.json
-DATA_DIR=$TEZOS_NODE_DATA/data/
+DATA_DIR=$TEZOS_NODE_DATA/data
 
-echo 'if data dir does not exist, create config and import snapshot'
-[ ! -d $TEZOS_NODE_DATA/data ] \
-&& echo 'initialize node' \
-&& tezos-node config --config-file=$CONFIG --network=$NETWORK init \
-&& tezos-node config --config-file=$CONFIG --rpc-addr=0.0.0.0:$RPC_PORT update \
-&& tezos-node config --config-file=$CONFIG --connections=100 --max-download-speed=1024 --max-upload-speed=1024 update \
+echo 'if data dir does not exist, import snapshot'
+[ ! -d $DATA_DIR  ] \
 && echo 'always get new snapshot on rebiuld' \
+&& mkdir $DATA_DIR \
 && wget $SNAPSHOT_URL -O $SNAPSHOT_PATH/$SNAPSHOT_FILE \
 && tezos-node snapshot import $SNAPSHOT_PATH/$SNAPSHOT_FILE --data-dir $DATA_DIR \
 || echo 'Data already exists'
+
+echo 'if config file does not exist, create it'
+[ ! -f $CONFIG ] \
+&& echo 'create new config file' \
+&& tezos-node config --config-file=$CONFIG --network=$NETWORK init \
+&& tezos-node config --config-file=$CONFIG --rpc-addr=0.0.0.0:$RPC_PORT update \
+&& tezos-node config --config-file=$CONFIG --connections=100 --max-download-speed=1024 --max-upload-speed=1024 update \
+|| echo 'Config file already exists; retain it'
 
 echo 'start node'
 tezos-node run --config-file=$CONFIG
