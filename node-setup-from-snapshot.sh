a96723cfbb54dd06573f9b763553976cd0e6a290
tezdealer <92530297+tezdealer@users.noreply.github.com>
2021-10-20 16:06:41 -0400

    Update node-setup-from-snapshot.sh
    Add data directory to config file. Seems like it's needed or else 'tezos-node run' sometimes uses default location, even though data-directory specified on snapshot import.
    Also, look for context directory to determine if data present to make it easier to manage config file separately.

--- node-setup-from-snapshot.sh
+++ node-setup-from-snapshot.sh
@@ -2,21 +2,21 @@
 CONFIG=$TEZOS_NODE_DATA/data/config.json
 DATA_DIR=$TEZOS_NODE_DATA/data
 
-echo 'if data dir does not exist, import snapshot'
-[ ! -d $DATA_DIR  ] \
-&& echo 'always get new snapshot on rebiuld' \
-&& mkdir $DATA_DIR \
-&& wget $SNAPSHOT_URL -O $SNAPSHOT_PATH/$SNAPSHOT_FILE \
-&& tezos-node snapshot import $SNAPSHOT_PATH/$SNAPSHOT_FILE --data-dir $DATA_DIR \
-|| echo 'Data already exists'
-
 echo 'if config file does not exist, create it'
 [ ! -f $CONFIG ] \
 && echo 'create new config file' \
 && tezos-node config --config-file=$CONFIG --network=$NETWORK init \
 && tezos-node config --config-file=$CONFIG --rpc-addr=0.0.0.0:$RPC_PORT update \
 && tezos-node config --config-file=$CONFIG --connections=100 --max-download-speed=1024 --max-upload-speed=1024 update \
+&& tezos-node config --config-file=$CONFIG --data-dir=$DATA_DIR update \
 || echo 'Config file already exists; retain it'
+
+echo 'if data dir does not exist, import snapshot'
+[ ! -d $DATA_DIR/context  ] \
+&& echo 'always get new snapshot on rebiuld' \
+&& wget $SNAPSHOT_URL -O $SNAPSHOT_PATH/$SNAPSHOT_FILE \
+&& tezos-node snapshot import $SNAPSHOT_PATH/$SNAPSHOT_FILE --data-dir $DATA_DIR \
+|| echo 'Data already exists'
 
 echo 'start node'
 tezos-node run --config-file=$CONFIG
