a942eac7b7afa71417d158c6ab2921d4e233d748
tezdealer <92530297+tezdealer@users.noreply.github.com>
2021-10-20 16:08:47 -0400

    Update start_mainnet.bat
    Perform build step separately to ensure non docker-compse.yml changes are incorporated, like changes to the start up script

--- start_mainnet.bat
+++ start_mainnet.bat
@@ -1,3 +1,4 @@
-docker-compose -f node-setup-from-snapshot.docker-compose.yml --env-file mainnet.env up -d --force-recreate
+docker-compose -f node-setup-from-snapshot.docker-compose.yml --env-file mainnet.env build --no-cache --force-rm
+docker-compose -f node-setup-from-snapshot.docker-compose.yml --env-file mainnet.env up -d --force-recreate --remove-orphans
 
 timeout 10
