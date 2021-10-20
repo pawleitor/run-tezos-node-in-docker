e21e9f85011e03fd07504ddb558fcf700a9f9b38
tezdealer <92530297+tezdealer@users.noreply.github.com>
2021-10-20 16:09:16 -0400

    Update start_mainnet.sh

--- start_mainnet.sh
+++ start_mainnet.sh
@@ -1,1 +1,2 @@
-docker-compose -f node-setup-from-snapshot.docker-compose.yml --env-file mainnet.env up -d --force-recreate
+docker-compose -f node-setup-from-snapshot.docker-compose.yml --env-file mainnet.env build --no-cache --force-rm
+docker-compose -f node-setup-from-snapshot.docker-compose.yml --env-file mainnet.env up -d --force-recreate --remove-orphans
