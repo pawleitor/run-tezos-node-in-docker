59d75ad865756249ec99122fb2b5c347fc5c4f7c
tezdealer <92530297+tezdealer@users.noreply.github.com>
2021-10-26 07:21:49 -0400

    Add files via upload
    check for id, create if missing. point to config file when generating it.

--- node-setup-from-snapshot.sh
+++ node-setup-from-snapshot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 CONFIG=$TEZOS_NODE_DATA/data/config.json
+ID=$TEZOS_NODE_DATA/data/identity.json
 DATA_DIR=$TEZOS_NODE_DATA/data
 
 echo 'if config file does not exist, create it'
@@ -7,16 +8,23 @@ echo 'if config file does not exist, create it'
 && echo 'create new config file' \
 && tezos-node config --config-file=$CONFIG --network=$NETWORK init \
 && tezos-node config --config-file=$CONFIG --rpc-addr=0.0.0.0:$RPC_PORT update \
-&& tezos-node config --config-file=$CONFIG --connections=100 --max-download-speed=1024 --max-upload-speed=1024 update \
 && tezos-node config --config-file=$CONFIG --data-dir=$DATA_DIR update \
 || echo 'Config file already exists; retain it'
 
+echo ''
 echo 'if data dir does not exist, import snapshot'
 [ ! -d $DATA_DIR/context  ] \
 && echo 'always get new snapshot on rebiuld' \
 && wget $SNAPSHOT_URL -O $SNAPSHOT_PATH/$SNAPSHOT_FILE \
 && tezos-node snapshot import $SNAPSHOT_PATH/$SNAPSHOT_FILE --data-dir $DATA_DIR \
-|| echo 'Data already exists'
+|| echo 'Data already exists; do not import snapshot'
 
+echo ''
+echo 'create new identity if needed'
+[ ! -f $ID ] \
+&& tezos-node identity generate 28 --config-file=$CONFIG \
+|| echo 'ID exists already'
+
+echo ''
 echo 'start node'
 tezos-node run --config-file=$CONFIG
